80aa29caa278c16c2f90cab46bb3159de669b944
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-12 16:30:22 +0100

    boolean operators 2

--- src/it/develhope/booleanOperators2/Main.java
+++ src/it/develhope/booleanOperators2/Main.java
@@ -3,13 +3,19 @@ package it.develhope.booleanOperators2;
 public class Main {
     public static void main(String[] args) {
 
-        boolean a = !(!(!(false ^ false) || (true && true))); //TRUE, false^false sarebbe false, ma viene cambiato dal
-        // NOT fuori parentesi, mentre true&&true è true, quindi il risultato è true, che diventerà false e poi
-        // nuovamente true grazie ai due operatori NOT
-        boolean b = !((3 * 2) <= 6) && (3 - 2 != 1); //FALSE, 3*2 è uguale a 6 quindi true, ma diventa false grazie al
-        // NOT, 3-2 non è diverso da 1 e quindi utilizzando AND si ottiene false
+
+        boolean a = (55 != 55) && (false ^ true); // FALSE, 55 non è diverso da 55, ed anche se false e true sono due
+        // risultati diversi, con AND sarà comunque false
+        boolean b = (!true || !false) || 2 == '2'; //TRUE, anche se 2=='2' darà risultato false, perché pur essendo
+        // entrambi 2 sono comunque due variabili diverse, il primo risultato è true
+        boolean c = false && true || false && !false; //FALSE, entrambi i valori paragonati hanno un elemento false e
+        // uno true, quindi entrambi avranno risultato false
+        boolean d = (false && true) || (false || true); //TRUE, il primo sarà false, ma il secondo, con l'operatore OR,
+        // avrà risultato true
 
         System.out.println(a);
         System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
